2c2172b315f35022f2c4fbd61c1d8cd2d17f72e4
Jin Choi <127823969+digitaltulbo@users.noreply.github.com>
2023-12-21 10:55:52 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,12 +38,12 @@ module "vpc" {
   enable_dns_hostnames = true
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${local.cluster_name}" = "owned" 
     "kubernetes.io/role/elb"                      = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${local.cluster_name}" = "owned"
     "kubernetes.io/role/internal-elb"             = 1
   }
 }
